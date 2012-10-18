f7712da6f12418d3ef7d4453095572e7ec902523
Louis-Mathieu Paquin <lmpaquin@gmail.com>
2012-10-18 13:20:51 -0400

    Default express app

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , fs = require('fs');
 
 var app = express();
 
@@ -29,6 +30,10 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/test', function(){
+   fs.mkdir("test-1212");
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
