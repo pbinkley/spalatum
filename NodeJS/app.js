5e9418329fdc6f68fe7dd37de2d0b9da2e194618
Peter Binkley <peter.binkley@ualberta.ca>
2012-10-20 11:16:56 -0400

    basic image conversion

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -20,7 +20,10 @@ app.configure(function(){
     app.set('view engine', 'jade');
     app.use(express.favicon());
     app.use(express.logger('dev'));
-    app.use(express.bodyParser());
+    app.use(express.bodyParser({
+    uploadDir: __dirname + '/../repo',
+    keepExtensions: true
+    }));
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
@@ -36,7 +39,48 @@ app.get('/', function(req, res){
 })
 app.get('/users', user.list);
 app.post('/ops/imgUpload', function(req, res){
-    res.send(console.dir(req.files));
+    //res.send(console.dir(req.files));
+    
+    /*
+    { image: 
+   { size: 25039,
+     path: '/Users/peterbinkley/data/spalatum/repo/b12c3f18861289206ad7168be7672ffb.png',
+     name: 'Screen Shot 2012-10-18 at ~ Oct 18 ~ 7.20AM 7.20.53 AM.png',
+     type: 'image/png',
+     hash: false,
+     lastModifiedDate: Sat Oct 20 2012 09:43:44 GMT-0400 (EDT),
+     _writeStream: 
+      { path: '/Users/peterbinkley/data/spalatum/repo/b12c3f18861289206ad7168be7672ffb.png',
+        fd: 10,
+        writable: false,
+        flags: 'w',
+        encoding: 'binary',
+        mode: 438,
+        bytesWritten: 25039,
+        busy: false,
+        _queue: [],
+        _open: [Function],
+        drainable: true },
+     length: [Getter],
+     filename: [Getter],
+     mime: [Getter] } }
+    */
+    
+    var imagefile = req.files.image.path;
+    var outputfile = 'test.jpg';
+    
+    var sys = require('sys')
+	var exec = require('child_process').exec;
+	exec("convert " + imagefile + " " + outputfile + "; identify " + outputfile, output);
+    function output(error, stdout, stderr) {
+    
+    
+    r = "<html><body><p>Size: " + req.files.image.size + "</p><p>Mime-type: " + 
+    	req.files.image.type + "</p><p>Convert result: " + stdout + "</p></body></html>";
+    res.send(r);
+    
+    }
+    
 });
 
 server.listen(3000);
@@ -59,7 +103,9 @@ io.sockets.on('connection', function(socket){
         console.log('User has disconnected. There are now ' + length + " users disconnected.");
     });
     socket.on('fileUpload', function(data){
-        console.log(data);
+//        console.log(data);
+//        console.log("bork");
+    //    console.log(req);
     //console.log(data);
     });
     
